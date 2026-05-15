538e96cd334efba28b5f74b09b38c9f65173196a
烬 <athendrakomin@coze.email>
2026-05-15 02:19:45 +0000

    fix: 还原缺失模块引用-移除NetworkStatus和还原index.tsx

--- client/web/src/components/ChatBox/ChatMessageList/index.tsx
+++ client/web/src/components/ChatBox/ChatMessageList/index.tsx
@@ -1,31 +1,30 @@
-import React, { useEffect } from 'react';
+import { LoadingSpinner } from '@/components/LoadingSpinner';
+import React from 'react';
+import { useSingleUserSetting } from 'tailchat-shared';
 import { NormalMessageList } from './NormalList';
-import { VirtualizedMessageList } from './VirtualizedList';
-import { useChatStore } from '@/store/chat';
-import { useSocketContext } from '@/context/SocketContext';
 import type { MessageListProps } from './types';
+import { VirtualizedMessageList } from './VirtualizedList';
 
-export const ChatMessageList: React.FC<MessageListProps> = React.memo((props) => {
-  const { converseId, messages, hasMore } = props;
-  const fetchHistoryMessages = useChatStore((state) => state.fetchHistoryMessages);
-  const socket = useSocketContext();
+export const ChatMessageList: React.FC<MessageListProps> = React.memo(
+  (props) => {
+    const { value: useVirtualizedList, loading } = useSingleUserSetting(
+      'messageListVirtualization',
+      false
+    );
 
-  useEffect(() => {
-    const handleConnect = () => {
-      if (converseId) {
-        fetchHistoryMessages(converseId);
-      }
-    };
+    if (loading) {
+      return (
+        <div className="flex-1">
+          <LoadingSpinner />
+        </div>
+      );
+    }
 
-    socket.on('connect', handleConnect);
-    return () => socket.off('connect', handleConnect);
-  }, [socket, converseId, fetchHistoryMessages]);
-
-  if (messages.length > 50) {
-    return <VirtualizedMessageList {...props} />;
+    return useVirtualizedList ? (
+      <VirtualizedMessageList {...props} />
+    ) : (
+      <NormalMessageList {...props} />
+    );
   }
-
-  return <NormalMessageList {...props} />;
-});
-
+);
 ChatMessageList.displayName = 'ChatMessageList';
